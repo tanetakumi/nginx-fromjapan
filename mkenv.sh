8679ead63d774311c64d09b6f8a3fb6dbfa1372f
Tane <75787495+tanetakumi@users.noreply.github.com>
2022-02-15 15:45:05 +0900

    Update mkenv.sh

--- mkenv.sh
+++ mkenv.sh
@@ -8,7 +8,7 @@ LF=$'\n'
 if [ ! -f $HOME/.env ]; then
     echo "Create a .env file."
     echo "# discordのwebhook${LF}DISCORD_WEBHOOK=${LF}${LF}# MyDNSの設定${LF}\
-MYDNS_USERNAME=${LF}MYDNS_PASSWORD=${LF}${LF}# 接続ip${LF}IP=" > $HOME/.env
+MYDNS_USERNAME=${LF}MYDNS_PASSWORD=${LF}${LF}# 接続ip${LF}IPADD=" > $HOME/.env
 else
     echo ".env file exists."
 fi
